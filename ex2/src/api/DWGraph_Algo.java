422de296c61a6b26b9f6e23361b9c1bcddeeb3ff
Shani Itzhakov <shani.it96@gmail.com>
2020-12-05 00:15:52 +0200

    Comments and javaDoc added and some code changes

--- ex2/src/api/DWGraph_Algo.java
+++ ex2/src/api/DWGraph_Algo.java
@@ -2,161 +2,229 @@ package api;
 
 import java.util.*;
 
+/**
+ * This class implements dw_graph_algorithms interface that represents
+ * a Directed (positive) Weighted Graph Theory Algorithms including:
+ * 0. clone(); (copy)
+ * 1. init(graph);
+ * 2. isConnected(); // strongly (all ordered pais connected)
+ * 3. double shortestPathDist(int src, int dest);
+ * 4. List<node_data> shortestPath(int src, int dest);
+ * 5. Save(file); // JSON file
+ * 6. Load(file); // JSON file
+ */
 public class DWGraph_Algo implements dw_graph_algorithms {
-    private directed_weighted_graph graph;
-    private HashMap<Integer, TagWeight> tags;
 
+	private directed_weighted_graph graph;
+	private HashMap<Integer, TagWeight> tags;
 
-    @Override
-    public void init(directed_weighted_graph g) {
-        this.graph = g;
-    }
+	/**
+	 * Init the graph on which this set of algorithms operates on.
+	 * @param directed_weighted_graph g
+	 */
+	@Override
+	public void init(directed_weighted_graph g) {
+		this.graph = g;
+	}
 
-    @Override
-    public directed_weighted_graph getGraph() {
-        return this.graph;
-    }
+	/**
+	 * Returns the underlying graph of which this class works.
+	 * @return directed_weighted_graph
+	 */
+	@Override
+	public directed_weighted_graph getGraph() {
+		return this.graph;
+	}
 
-    @Override
-    public directed_weighted_graph copy() {
-        if (this.graph != null){
-            return new DWGraph_DS(this.graph);
+	/**
+	 * Compute a deep copy of this weighted graph.
+	 * @return directed_weighted_graph
+	 */
+	@Override
+	public directed_weighted_graph copy() {
+		if (this.graph != null){
+			return new DWGraph_DS(this.graph);
 
-        }
-        return null;
-    }
+		}
+		return null;
+	}
 
-    @Override
-    public boolean isConnected() {
-        if (graph == null || graph.getV().size() <= 1) {
-            return true;
-        }
-        for (node_data node : graph.getV()) {
-            Dijkstra(node.getKey(),null);
-            if (tags.size() != graph.nodeSize()){
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * Returns true if and only if there is a valid path from each node to each
+	 * other node. NOTE: assume directional graph (all n*(n-1) ordered pairs).
+	 * @return boolean
+	 */
+	@Override
+	public boolean isConnected() {
+		if (graph == null || graph.getV().size() <= 1) {
+			return true;
+		}
+		for (node_data node : graph.getV()) {
+			Dijkstra(node.getKey(),null);
+			if (tags.size() != graph.nodeSize()){
+				return false;
+			}
+		}
+		return true;
+	}
 
-    @Override
-    public double shortestPathDist(int src, int dest) {
-        if (graph == null || graph.getNode(src) == null || graph.getNode(dest) == null){
-            return -1;
-        }
-        if (src == dest) {
-            return 0;
-        }
-        Dijkstra(src,dest);
-        if (!tags.containsKey(dest)) {
-            return -1;
-        }
-        return tags.get(dest).getSumWeight();
-    }
+	/**
+	 * Returns the length of the shortest path between src to dest,
+	 * If no such path --> returns -1.
+	 * @param src - start node
+	 * @param dest - end (target) node
+	 * @return double
+	 */
+	@Override
+	public double shortestPathDist(int src, int dest) {
+		if (graph == null || graph.getNode(src) == null || graph.getNode(dest) == null){
+			return -1;
+		}
+		if (src == dest) {
+			return 0;
+		}
+		Dijkstra(src,dest);
+		if (!tags.containsKey(dest)) {
+			return -1;
+		}
+		return tags.get(dest).getSumWeight();
+	}
 
-    @Override
-    public List<node_data> shortestPath(int src, int dest) {
-        if (shortestPathDist(src, dest) == -1 || graph.getV().isEmpty()) {
-            return null;
-        }
-        List<node_data> list = new ArrayList<>();
-        if (src == dest) {
-            list.add(graph.getNode(src));
-            return list;
-        }
-        node_data destNode = graph.getNode(dest);
-        list.add(destNode);
-        boolean finished = false;
-        int nextNodeIndex = 0;
-        while (!finished) { // while loop, adds shortest path from dest to src
-            node_data node = list.get(nextNodeIndex);
-            list.add(tags.get(node.getKey()).getParent());
-            nextNodeIndex++;
-            if(list.get(list.size() - 1) == graph.getNode(src)){
-                finished = true;
-            }
-        }
-        Collections.reverse(list);
-        return list;
-    }
+	/**
+	 * Returns the shortest path between src to dest - as an ordered List of nodes:
+	 * src--> n1-->n2-->...dest
+	 * If no such path --> returns null.
+	 * @param src - start node
+	 * @param dest - end (target) node
+	 * @return List<node_data>
+	 */
+	@Override
+	public List<node_data> shortestPath(int src, int dest) {
+		if (shortestPathDist(src, dest) == -1 || graph.getV().isEmpty()) {
+			return null;
+		}
+		List<node_data> list = new ArrayList<>();
+		if (src == dest) {
+			list.add(graph.getNode(src));
+			return list;
+		}
+		node_data destNode = graph.getNode(dest);
+		list.add(destNode);
+		boolean finished = false;
+		int nextNodeIndex = 0;
+		// while loop, adds shortest path from dest to src
+		while (!finished) {
+			node_data node = list.get(nextNodeIndex);
+			list.add(tags.get(node.getKey()).getParent());
+			nextNodeIndex++;
+			if(list.get(list.size() - 1) == graph.getNode(src)){
+				finished = true;
+			}
+		}
+		Collections.reverse(list);
+		return list;
+	}
 
-    private void Dijkstra(int src, Integer dest) {
-        tags = new HashMap<>();
+	/** 
+	 * Private function that uses Dijkstra algorithm to find the shortest path
+	 * according to the weight.
+	 * @param int src
+	 * @param Integer dest
+	 */
+	private void Dijkstra(int src, Integer dest) {
+		tags = new HashMap<>();
 
-        node_data nodeSrc = graph.getNode(src);
-        Queue<node_data> q = new PriorityQueue<>(new WeightComparator());
-        q.add(nodeSrc);
-        TagWeight tag = new TagWeight(null, 0);
-        tags.put(src, tag);
-        while (!q.isEmpty()) {
-            node_data node = q.poll();
-            for (edge_data edge : graph.getE(node.getKey())) {
-                double sumWeight = edge.getWeight() + tags.get(node.getKey()).getSumWeight();
-                node_data neighbor = graph.getNode(edge.getDest());
-                if (!tags.containsKey(neighbor.getKey()) && !q.contains(neighbor)) {
-                    TagWeight t = new TagWeight(node, sumWeight);
-                    tags.put(edge.getDest(), t);
-                    q.add(neighbor);
-                }
-                else if (sumWeight < tags.get(edge.getDest()).getSumWeight()) {
-                    TagWeight t = new TagWeight(node, sumWeight);
-                    tags.put(edge.getDest(), t);
-                }
-                if (dest != null && node.getKey() == dest){
-                    return;
-                }
-            }
-        }
-    }
+		node_data nodeSrc = graph.getNode(src);
+		Queue<node_data> q = new PriorityQueue<>(new WeightComparator());
+		q.add(nodeSrc);
+		TagWeight tag = new TagWeight(null, 0);
+		tags.put(src, tag);
+		while (!q.isEmpty()) {
+			node_data node = q.poll();
+			for (edge_data edge : graph.getE(node.getKey())) {
+				double sumWeight = edge.getWeight() + tags.get(node.getKey()).getSumWeight();
+				node_data neighbor = graph.getNode(edge.getDest());
+				if (!tags.containsKey(neighbor.getKey()) && !q.contains(neighbor)) {
+					TagWeight t = new TagWeight(node, sumWeight);
+					tags.put(edge.getDest(), t);
+					q.add(neighbor);
+				}
+				else if (sumWeight < tags.get(edge.getDest()).getSumWeight()) {
+					TagWeight t = new TagWeight(node, sumWeight);
+					tags.put(edge.getDest(), t);
+				}
+				if (dest != null && node.getKey() == dest){
+					return;
+				}
+			}
+		}
+	}
 
-    @Override
-    public boolean save(String file) {
-        return false;
-    }
+	/**
+	 * Saves this weighted (directed) graph to the given
+	 * file name - in JSON format
+	 * @param file - the file name (may include a relative path).
+	 * @return true - iff the file was successfully saved
+	 */
+	@Override
+	public boolean save(String file) {
+		return false;
+	}
 
-    @Override
-    public boolean load(String file) {
-        return false;
-    }
+	/**
+	 * This method load a graph to this graph algorithm.
+	 * if the file was successfully loaded - the underlying graph
+	 * of this class will be changed (to the loaded one), in case the
+	 * graph was not loaded the original graph should remain "as is".
+	 * @param file - file name of JSON file
+	 * @return true - if and only if the graph was successfully loaded.
+	 */
+	@Override
+	public boolean load(String file) {
+		return false;
+	}
 
-    private class WeightComparator implements Comparator<node_data> {
+	/**
+	 * This class implements Comparator interface and compare weights
+	 * that are used to save the distance from src.
+	 * This is needed for the priority queue.
+	 */
+	private class WeightComparator implements Comparator<node_data> {
 
-        /**
-         * Overrides compare method by tag (weight), if node1 > node2 return 1, if node1 < node2 return -1, else return 0.
-         * used in the priorityQueue
-         * @param node1 - node1 to compare
-         * @param node2 - node2 to compare
-         * @return - int if node1 > node2 return 1, if node1 < node2 return -1, else return 0.
-         */
+		/**
+		 * Overrides compare method by tag (weight), if node1 > node2 return 1, if node1 < node2 return -1, else return 0.
+		 * used in the priorityQueue.
+		 * @param node1 - node1 to compare
+		 * @param node2 - node2 to compare
+		 * @return - int if node1 > node2 return 1, if node1 < node2 return -1, else return 0.
+		 */
+		@Override
+		public int compare(node_data node1, node_data node2) {
+			double sum1 = tags.get(node1.getKey()).getSumWeight();
+			double sum2 = tags.get(node2.getKey()).getSumWeight();
+			if (sum1 > sum2) {
+				return 1;
+			} else if (sum1 <sum2) {
+				return -1;
+			}
+			return 0;
+		}
+	}
 
-        @Override
-        public int compare(node_data node1, node_data node2) {
-            double sum1 = tags.get(node1.getKey()).getSumWeight();
-            double sum2 = tags.get(node2.getKey()).getSumWeight();
-            if (sum1 > sum2) {
-                return 1;
-            } else if (sum1 <sum2) {
-                return -1;
-            }
-            return 0;
-        }
-    }
+	private class TagWeight {
+		private double sumWeight;
+		private node_data parent;
 
-  private class TagWeight {
-        private double sumWeight;
-        private node_data parent;
+		public TagWeight(node_data parent, double sumWeight){
+			this.parent = parent;
+			this.sumWeight = sumWeight;
+		}
+		public double getSumWeight(){
+			return sumWeight;
+		}
 
-        public TagWeight(node_data parent, double sumWeight){
-            this.parent = parent;
-            this.sumWeight = sumWeight;
-        }
-        public double getSumWeight(){
-            return sumWeight;
-        }
-
-        public node_data getParent() {
-            return parent;
-        }
-    }
+		public node_data getParent() {
+			return parent;
+		}
+	}
 }
--- ex2/src/api/DWGraph_DS.java
+++ ex2/src/api/DWGraph_DS.java
@@ -5,155 +5,235 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * This class implements directed_weighted_graph interface
+ * that represents a directional weighted graph.
+ * The interface has a road-system or communication network in mind.
+ */
 public class DWGraph_DS implements directed_weighted_graph {
-    private HashMap<Integer, node_data> nodes;
-    private HashMap<Integer, HashMap<Integer, edge_data>> neighbors;
-    private HashMap<Integer, List<Integer>> inEdges; // Point of this is to point to the existing edge and not create a new one
-    private int edgeSize, mc;                                      // so in connect i also need to update this with the existing hashmap of
-                                                                   // edges.
 
+	// This HashMap holds the nodes of this graph
+	private HashMap<Integer, node_data> nodes = new HashMap<>();
+	// This HashMap holds the node neighbors and the edges between them 
+	private HashMap<Integer, HashMap<Integer, edge_data>> neighbors = new HashMap<>();
+	// This HashMap hold the parents of nodes in the graph
+	private HashMap<Integer, List<Integer>> parents = new HashMap<>();
+	private int edgeSize, mc;
+	
+	/**
+	 * Default constructor.
+	 */
+	public DWGraph_DS() {
+		this.edgeSize = 0;
+		this.mc = 0;
+	}
 
-    public DWGraph_DS() {
-        this.nodes = new HashMap<>();
-        this.neighbors = new HashMap<>();
-        this.inEdges = new HashMap<>();
-        this.edgeSize = 0;
-        this.mc = 0;
-    }
+	/**
+	 * Deep copy constructor that copies
+	 * an existing graph and creates a new graph.
+	 */
+	public DWGraph_DS(directed_weighted_graph g) {
+		// Check if graph is null else copy
+		if (g == null) {
+			return;
+		}
+		// Loop and create new nodes and copy content from each node
+		for (node_data n : g.getV()) {
+			int srcKey = n.getKey();
+			this.nodes.put(srcKey, new NodeData(srcKey));
+		}
+		// Loop over the nodes and create new nodes, then copy content from each node
+		for (node_data node : g.getV()) {
+			int srcKey = node.getKey();
+			// Loop through the edges of each node
+			for (edge_data edge: g.getE(srcKey)) {
+				int destKey = edge.getDest();
+				double weight = edge.getWeight();
+				// Connect an edge with two nodes and their weight
+				connect(srcKey, destKey, weight);
+			}
+		}
+		// Set the mode counter and edge size to the same value as the copied graph
+		this.edgeSize = g.edgeSize();
+		this.mc = g.getMC();
+	}
 
-    public DWGraph_DS(directed_weighted_graph g) {
-        // Check if graph is null else copy
-        if (g == null) {
-            return;
-        }
-        this.nodes = new HashMap<>();
-        this.neighbors = new HashMap<>();
-        this.inEdges = new HashMap<>();
-        for (node_data n : g.getV()) { // loop and create new nodes and copy content from each node
-            node_data a = new NodeData(n.getKey());
-            this.nodes.put(a.getKey(), a);
-        }
-        for (node_data n : g.getV()) {
-            for (edge_data edge: g.getE(n.getKey())) { // loop through the neighbors of each node
-                    int nodeSrc = n.getKey();
-                    int nodeDest = edge.getDest();
-                    double weight = edge.getWeight();
-                    connect(nodeSrc, nodeDest, weight); // connect an edge with two nodes
-            }
-        }
-        this.edgeSize = g.edgeSize();
-        this.mc = g.getMC();
-    }
+	/**
+	 * Returns the node_data by the node_id,
+	 * @param key - the node_id
+	 * @return the node_data by the node_id, null if none.
+	 */
+	@Override
+	public node_data getNode(int key) {
+		return nodes.get(key);
+	}
 
-    @Override
-    public node_data getNode(int key) {
-        return nodes.get(key);
-    }
+	/**
+	 * Returns the data of the edge (src,dest), null if none.
+	 * @param int src
+	 * @param int dest
+	 * @return edge_data edge
+	 */
+	@Override
+	public edge_data getEdge(int src, int dest) {
+		// Check if node src and dest exists in the graph
+		if (src != dest && nodes.get(src) != null && nodes.get(dest) != null) {
+			if (neighbors.containsKey(src)) {
+				return neighbors.get(src).get(dest);
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public edge_data getEdge(int src, int dest) {
-        node_data nodeSrc = nodes.get(src);
-        node_data nodeDest = nodes.get(dest);
-        if (nodeSrc != null && nodeDest != null) {
-            if (neighbors.containsKey(src)) {
-                return neighbors.get(src).get(dest);
-            }
-        }
-        return null;
-    }
+	/**
+	 * Adds a new node to the graph with the given node_data.
+	 * @param node_data n
+	 */
+	@Override
+	public void addNode(node_data n) {
+		if (!nodes.containsKey(n.getKey())) {
+			nodes.put(n.getKey(), n);
+			mc++;
+		}
+	}
 
-    @Override
-    public void addNode(node_data n) {
-        if (!nodes.containsKey(n.getKey())) {
-            nodes.put(n.getKey(), n);
-            mc++;
-        }
-    }
+	/**
+	 * Connects an edge with weight w between node src to node dest.
+	 * @param src - the source of the edge.
+	 * @param dest - the destination of the edge.
+	 * @param w - positive weight representing the cost (aka time, price, etc) between src-->dest.
+	 */
+	@Override
+	public void connect(int src, int dest, double w) {
+		// Check if src is not equal to dest and if they exist in the graph
+		// and if the weight is positive, then connect
+		if (src != dest && nodes.get(src) != null && nodes.get(dest) != null && w >= 0) {
+			// If src does not have neighbors, then create a new inner hash map and put it in neighbors
+			if (!neighbors.containsKey(src)) {
+				HashMap<Integer, edge_data> map = new HashMap<>();
+				neighbors.put(src, map);
+			}
+			// If dest does not have parents, then create a new list and put it in parents
+			if (!parents.containsKey(dest)) {
+				List<Integer> list = new ArrayList<>();
+				parents.put(dest, list);
+			}
+			// If src and dest are not neighbors, then connect them
+			if (!neighbors.get(src).containsKey(dest)) {
+				edge_data edge = new EdgeData(src, dest, w);
+				neighbors.get(src).put(dest, edge);
+				parents.get(dest).add(src);
+				edgeSize++;
+				mc++;	
+			}
+			// If src and dest are already neighbors, then update the weight
+			else if (neighbors.get(src).get(dest).getWeight() != w) {
+				edge_data edge = new EdgeData(src, dest, w);
+				neighbors.get(src).replace(dest, edge);
+				mc++;
+			}
+		}
+	}
 
-    @Override
-    public void connect(int src, int dest, double w) {
-        node_data nodeSrc = nodes.get(src);
-        node_data nodeDest = nodes.get(dest);
-        if (nodeSrc != null && nodeDest != null && w >= 0 && nodeSrc != nodeDest) {
-            if (!neighbors.containsKey(src)) {
-                HashMap<Integer, edge_data> map = new HashMap<>();
-                neighbors.put(src, map);
-            }
-            if (!inEdges.containsKey(dest)) {
-                List<Integer> list = new ArrayList<>();
-                inEdges.put(dest, list);
-            }
-            if (!neighbors.get(src).containsKey(dest)) {
-                edge_data edge = new EdgeData(src, dest, w);
-                neighbors.get(src).put(dest, edge);
-                inEdges.get(dest).add(src);
-                edgeSize++;
-                mc++;
-            } else if (neighbors.get(src).get(dest).getWeight() != w) {
-                edge_data edge = new EdgeData(src, dest, w);
-                neighbors.get(src).replace(dest, edge);
-                mc++;
-            }
-        }
-    }
+	/**
+	 * This method returns a pointer (shallow copy) for the
+	 * collection representing all the nodes in the graph. 
+	 * @return Collection<node_data>
+	 */
+	@Override
+	public Collection<node_data> getV() {
+		return nodes.values();
+	}
 
-    @Override
-    public Collection<node_data> getV() {
-        return nodes.values();
-    }
+	/**
+	 * This method returns a pointer (shallow copy) for the
+	 * collection representing all the edges getting out of 
+	 * the given node (all the edges starting (source) at the given node). 
+	 * Note: this method should run in O(k) time, k being the collection size.
+	 * @return Collection<edge_data>
+	 */
+	@Override
+	public Collection<edge_data> getE(int node_id) {
+		if (!neighbors.containsKey(node_id)) {
+			return new HashMap<Integer, edge_data>().values();
+		}
+		return neighbors.get(node_id).values();
+	}
 
-    @Override
-    public Collection<edge_data> getE(int node_id) {
-        if (!neighbors.containsKey(node_id)) {
-            return new HashMap<Integer, edge_data>().values();
-        }
-        return neighbors.get(node_id).values();
-    }
+	/**
+	 * Deletes the node (with the given ID) from the graph -
+	 * and removes all edges which starts or ends at this node.
+	 * This method should run in O(k), V.degree=k, as all the edges should be removed.
+	 * @return the data of the removed node (null if none). 
+	 * @param key
+	 */
+	@Override
+	public node_data removeNode(int key) {
+		node_data node = nodes.get(key);
+		// If node is not in graph, then remove node
+		if (node != null) {
+			int size = getE(key).size();
+			// Loop over the edges size and remove the edge between key and dest
+			for (int i = 0; i < size; i++) {
+				removeEdge(key,getE(key).iterator().next().getDest());
+			}
+			// Loop over the src from the parents map and remove their edges
+			for (int src : parents.get(key)) {
+				removeEdge(src, key);
+			}
+			// Remove the node from the list of nodes in the graph
+			nodes.remove(key);
+			mc++;
+			return node;
+		}
+		return null;
+	}
 
-    @Override
-    public node_data removeNode(int key) {
-        node_data node = nodes.get(key);
-        if (node != null) {
-            int size = getE(key).size();
-            for (int i = 0; i < size; i++) {
-                removeEdge(key,getE(key).iterator().next().getDest());
-            }
-            for (int src : inEdges.get(key)) {
-                removeEdge(src,key);
-            }
-            nodes.remove(key);
-            mc++;
-            return node;
-        }
-        return null;
-    }
+	/**
+	 * Deletes the edge from the graph,
+	 * @param int src
+	 * @param int dest
+	 * @return edge_data - the data of the removed edge (null if none).
+	 */
+	@Override
+	public edge_data removeEdge(int src, int dest) {
+		// Check if src is not equal to dest and if they exist in the graph
+		if (src != dest && nodes.get(dest) != null && nodes.get(src) != null) {
+			// Check if src has neighbors and if dest is its neighbor
+			if (neighbors.containsKey(src) && neighbors.get(src).containsKey(dest)) {
+				edgeSize--;
+				mc++;
+				//parents.get(dest).remove(src);
+				return neighbors.get(src).remove(dest);
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public edge_data removeEdge(int src, int dest) {
-        node_data nodeSrc = nodes.get(src);
-        node_data nodeDest = nodes.get(dest);
-        if (nodeDest != null && nodeSrc != null) {
-            if (neighbors.containsKey(src) && neighbors.get(src).containsKey(dest)) {
-                edgeSize--;
-                mc++;
-                return neighbors.get(src).remove(dest);
-            }
-        }
-        return null;
-    }
+	/** 
+	 * Returns the number of vertices (nodes) in the graph.
+	 * @return int node size
+	 */
+	@Override
+	public int nodeSize() {
+		return nodes.size();
+	}
 
-    @Override
-    public int nodeSize() {
-        return nodes.size();
-    }
+	/** 
+	 * Returns the number of edges (assume directional graph).
+	 * @return int edge size
+	 */
+	@Override
+	public int edgeSize() {
+		return edgeSize;
+	}
 
-    @Override
-    public int edgeSize() {
-        return edgeSize;
-    }
-
-    @Override
-    public int getMC() {
-        return mc;
-    }
+	/**
+	 * Returns the Mode Count - for testing changes in the graph.
+	 * @return int mode counter
+	 */
+	@Override
+	public int getMC() {
+		return mc;
+	}
 }
